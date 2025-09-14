1c0c413c3ce4eb3ec717f0db9afcec683cb6da94
davidlin17 <davidkedua7@gmail.com>
2025-09-14 20:29:58 +0700

    add coffe list and detail fragment

--- app/src/main/java/com/example/lab_week_03/DetailFragment.kt
+++ app/src/main/java/com/example/lab_week_03/DetailFragment.kt
@@ -1,42 +1,40 @@
-package com.example.lab_week_03
+override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
+    inflater.inflate(R.layout.fragment_detail, container, false)
 
-import android.os.Bundle
-import android.view.*
-import android.widget.TextView
-import androidx.fragment.app.Fragment
+fun setCoffeeData(id: Int) {
+    when (id) {
+        R.id.affogato -> {
+            coffeeTitle?.text = getString(R.string.affogato_title)
+            coffeeDesc?.text  = getString(R.string.affogato_desc)
+        }
+        R.id.americano -> {
+            coffeeTitle?.text = getString(R.string.americano_title)
+            coffeeDesc?.text  = getString(R.string.americano_desc)
+        }
+        R.id.latte -> {
+            coffeeTitle?.text = getString(R.string.latte_title)
+            coffeeDesc?.text  = getString(R.string.latte_desc)
+        }
+        R.id.cappuccino -> {
+            coffeeTitle?.text = getString(R.string.cappuccino_title)
+            coffeeDesc?.text  = getString(R.string.cappuccino_desc)
+        }
+        R.id.mocha -> {
+            coffeeTitle?.text = getString(R.string.mocha_title)
+            coffeeDesc?.text  = getString(R.string.mocha_desc)
+        }
+        override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val coffeeId = arguments?.getInt(COFFEE_ID, 0) ?: 0
+        setCoffeeData(coffeeId)
+    }
 
-class DetailFragment : Fragment() {
+        fun setCoffeeData(id: Int) { /* (sama seperti Part 2) */ }
 
-    private val coffeeTitle get() = view?.findViewById<TextView>(R.id.coffee_title)
-    private val coffeeDesc  get() = view?.findViewById<TextView>(R.id.coffee_desc)
-
-    override fun onCreate(savedInstanceState: Bundle?) { super.onCreate(savedInstanceState) }
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
-        inflater.inflate(R.layout.fragment_detail, container, false)
-
-    fun setCoffeeData(id: Int) {
-        when (id) {
-            R.id.affogato -> {
-                coffeeTitle?.text = getString(R.string.affogato_title)
-                coffeeDesc?.text  = getString(R.string.affogato_desc)
-            }
-            R.id.americano -> {
-                coffeeTitle?.text = getString(R.string.americano_title)
-                coffeeDesc?.text  = getString(R.string.americano_desc)
-            }
-            R.id.latte -> {
-                coffeeTitle?.text = getString(R.string.latte_title)
-                coffeeDesc?.text  = getString(R.string.latte_desc)
-            }
-            R.id.cappuccino -> {
-                coffeeTitle?.text = getString(R.string.cappuccino_title)
-                coffeeDesc?.text  = getString(R.string.cappuccino_desc)
-            }
-            R.id.mocha -> {
-                coffeeTitle?.text = getString(R.string.mocha_title)
-                coffeeDesc?.text  = getString(R.string.mocha_desc)
+                companion object {
+            private const val COFFEE_ID = "COFFEE_ID"
+            fun newInstance(id: Int) = DetailFragment().apply {
+                arguments = Bundle().apply { putInt(COFFEE_ID, id) }
             }
         }
     }
-}
--- app/src/main/java/com/example/lab_week_03/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_03/MainActivity.kt
@@ -1,38 +1,39 @@
-package com.example.lab_week_03
-
-import android.os.Bundle
-import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
-    interface CoffeeListener { fun onSelected(id: Int) }
+interface CoffeeListener { fun onSelected(id: Int) }
+import androidx.fragment.app.FragmentContainerView
 
-    class MainActivity : AppCompatActivity(), CoffeeListener {
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            enableEdgeToEdge()
-            setContentView(R.layout.activity_main)
-            ViewCompat.setOnApplyWindowInsetsListener(findViewById(android.R.id.content)) { v, insets ->
-                ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-                    val sb = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-                    v.setPadding(sb.left, sb.top, sb.right, sb.bottom)
-                    insets
-                    v.setPadding(sb.left, sb.top, sb.right, sb.bottom); insets
-                }
-                Log.d(TAG, "onCreate")
+class MainActivity : AppCompatActivity(), CoffeeListener {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.fragment_container)) { v, insets ->
+                val sb = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+                v.setPadding(sb.left, sb.top, sb.right, sb.bottom); insets
             }
-            override fun onStart() { super.onStart(); Log.d(TAG, "onStart") }
-            override fun onResume() { super.onResume(); Log.d(TAG, "onResume") }
-            override fun onPause() { super.onPause(); Log.d(TAG, "onPause") }
-            override fun onStop() { super.onStop(); Log.d(TAG, "onStop") }
-            override fun onDestroy() { super.onDestroy(); Log.d(TAG, "onDestroy") }
-
-            companion object { private const val TAG = "MainActivity" }
-            override fun onSelected(id: Int) {
-                val detail = supportFragmentManager.findFragmentById(R.id.fragment_detail) as DetailFragment
-                detail.setCoffeeData(id)
+            if (savedInstanceState == null) {
+                findViewById<FragmentContainerView>(R.id.fragment_container).let { container ->
+                    supportFragmentManager.beginTransaction()
+                        .add(container.id, ListFragment())
+                        .commit()
+                }
             }
         }
+
+        override fun onSelected(id: Int) {
+            val detail = supportFragmentManager.findFragmentById(R.id.fragment_detail) as DetailFragment
+            detail.setCoffeeData(id)
+            findViewById<FragmentContainerView>(R.id.fragment_container).let { container ->
+                val detail = DetailFragment.newInstance(id)
+                supportFragmentManager.beginTransaction()
+                    .replace(container.id, detail)
+                    .addToBackStack(null)
+                    .commit()
+            }
+        }
+    }
